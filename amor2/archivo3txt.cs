dbb47233b5a30b09d55b6e2effb5cdaa2fdbe9ce
Lucas Miranda <alumnosanalfonso@gmail.com>
2025-11-05 21:52:56 -0300

    agregar nuevos commits"

---
+++ amor2/archivo3txt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace amor2
+{
+    internal class archivo3txt
+    {
+        public archivo3txt() {
+            Console.WriteLine("Hola mundo desde archivo3txt");
+        }
+    }
+}
